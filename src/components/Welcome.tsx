e17611b37d28db4f9b2565d4bb4f60d327801849
Mateen Afshari <mateen.afshari@gmail.com>
2023-11-24 14:24:12 -0500

    change button

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -22,17 +22,22 @@ function Welcome() {
           with friends.
         </p>
 
-        <div className="text-5xl flex justify-center gap-10 pb-10 sm:pb-0">
-          <a href="https://github.com/mateen-afshari" target="_blank">
+        <div className=" flex justify-center align-middle gap-10 pb-10 sm:pb-0">
+          <a
+            href="https://github.com/mateen-afshari"
+            target="_blank"
+            className="text-5xl pt-2"
+          >
             <FontAwesomeIcon icon={faGithub} color="white" />
           </a>
           <a
             href="https://www.linkedin.com/in/mateen-afshari-90598022b/"
             target="-blank"
+            className="text-5xl pt-2"
           >
             <FontAwesomeIcon icon={faLinkedin} color="white" />
           </a>
-          <button
+          {/* <button
             className="bg-sky-500 hover:bg-sky-600 text-white px-3 rounded"
             onClick={() => {
               window.open(
@@ -58,7 +63,49 @@ function Welcome() {
               </svg>
               <h3 className="m-2 text-lg">Resume</h3>
             </div>
-          </button>
+          </button> */}
+          <a
+            href="https://drive.google.com/file/d/16KJbh1qVBAhOi_M6TyMnxDWFbXbevHeO/view?usp=sharing"
+            target="_blank"
+            className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-sky-600 transition duration-300 ease-out border-2 border-sky-500 rounded-full shadow-md group"
+          >
+            <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-sky-500 group-hover:translate-x-0 ease">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                ></path>
+              </svg>
+            </span>
+            <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                  />
+                </svg>
+                <h3 className="m-2 text-lg">Resume</h3>
+              </div>
+            </span>
+            <span className="relative invisible">Button Text</span>
+          </a>
         </div>
       </div>
     </div>
